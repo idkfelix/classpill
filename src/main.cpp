52dadaa9a875b9b8f4cb98b39dff53d8f59516cc
idkfelix <felix.coulthard@gmail.com>
2023-08-14 14:53:42 +1000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,20 @@
 #include "esp_rom_gpio.h"
 #include "driver/gpio.h"
 #include "TFT_eSPI.h"
+#include <EEPROM.h>
+#include <StreamUtils.h>
 
 WiFiMulti wifiMulti;
 HTTPClient http;
 CookieJar cookieJar;
-
 TFT_eSPI tft = TFT_eSPI();
 
-bool fetched = false;
-String userId = "";
+
 JsonArray arr;
-String data = "";
-String html = "";
 
 void setup() {
+  String userId = "";
+
   // Start Serial
   Serial.begin(115200);
   Serial.printf("\n\n\n\n\n");
@@ -28,12 +28,15 @@ void setup() {
   tft.init();
   tft.setRotation(3);
 
+  // Start EEPROM
+  EEPROM.begin(1000);
+
   // Configure HTTP
   http.setCookieJar(&cookieJar);
   http.setReuse(true);
 
   // Define Networks here
-  wifiMulti.addAP("ssid", "pass");
+  wifiMulti.addAP("Felix", "idkidkidk");
   WiFi.setHostname("classpill");
 
   tft.fillScreen(TFT_BLACK);
@@ -53,13 +56,15 @@ void setup() {
     tft.drawCentreString("Authenticating...",64,64,1);
     http.begin("https://mullauna-vic.compass.education/services/admin.svc/AuthenticateUserCredentials");
     http.addHeader("Content-Type", "application/json");
-    int httpCodeA = http.POST("{\"username\":\"\",\"password\":\"\"}");
+    int httpCodeA = http.POST("{\"username\":\"cou0008\",\"password\":\"Lion.8664\"}");
 
     if(httpCodeA > 0) {
       if(httpCodeA == HTTP_CODE_OK) {
+        // Convert response to JSON
         String payload = http.getString();
         DynamicJsonDocument doc(1024);
         deserializeJson(doc, payload);
+        // Get and print userId
         userId = doc["d"]["roles"][0]["userId"].as<String>();
         Serial.printf("User ID: %s\n\n",userId);
       } else {
@@ -76,21 +81,26 @@ void setup() {
       tft.drawCentreString("Fetching Schedule...",64,64,1);
       http.begin("https://mullauna-vic.compass.education/Services/mobile.svc/GetScheduleLinesForDate");
       http.addHeader("Content-Type", "application/json");
-      int httpCodeB = http.POST("{\"userId\":\""+userId+"\",\"date\":\"11/08/2023 - 10:00 AM\"}");
+      int httpCodeB = http.POST("{\"userId\":\""+userId+"\",\"date\":\"14/08/2023 - 10:00 AM\"}");
 
       if(httpCodeB > 0) {
         if(httpCodeB == HTTP_CODE_OK) {
         String payload = http.getString();
+          // Filter and deserialise JSON
           StaticJsonDocument<64> filter;
           filter["d"]["data"][0]["topAndBottomLine"] = true;
           DynamicJsonDocument doc(1024);
           deserializeJson(doc, payload, DeserializationOption::Filter(filter));
           arr = doc["d"]["data"].as<JsonArray>();
+          // Print to serial
           for (JsonVariant period : arr) {
             String s = (String)period["topAndBottomLine"].as<const char*>();
             Serial.println(s);
           }
-          serializeJson(doc["d"]["data"], data);
+          // Save to memory
+          EepromStream eepromStream(0, 1024);
+          serializeJson(doc, eepromStream);
+          eepromStream.flush();
         } else {
           Serial.printf("%i\n",httpCodeB);
         }
@@ -99,13 +109,33 @@ void setup() {
       }
       http.end();
     }
-  } 
+  } else {
+    tft.fillScreen(TFT_BLACK);
+    tft.drawCentreString("No WiFi found!",64,64,1);
+
+    DynamicJsonDocument doc(1024);
+    EepromStream eepromStream(0, 1024);
+    deserializeJson(doc, eepromStream);
+    arr = doc["d"]["data"].as<JsonArray>();
+    // Print to serial
+    for (JsonVariant period : arr) {
+      String s = (String)period["topAndBottomLine"].as<const char*>();
+      Serial.println(s);
+    }
+    tft.fillScreen(TFT_BLACK);
+    tft.drawCentreString("Fetched Cached Data",64,64,1);
+  }
+  tft.fillScreen(TFT_BLACK);
 }
 
 void loop(){
-  for (JsonVariant period : arr) {
-    String s = (String)period["topAndBottomLine"].as<const char*>();
-    tft.drawCentreString(s,64,64,2);
-    delay(1000);
+  tft.setTextWrap(true, true);
+  if(arr.size() >0){
+    for(JsonVariant period : arr) {
+      tft.println(period["topAndBottomLine"].as<const char*>());
+    }
+  } else {
+    tft.drawCentreString("No Period!",64,64,1);
   }
+  while(1){}
 }
