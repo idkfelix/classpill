c4f74cca58e1e0238eb64a9c7a59f5a24b91d18f
idkfelix <felix.coulthard@gmail.com>
2023-08-15 19:14:23 +1000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
+
+#include <EEPROM.h>
+#include <StreamUtils.h>
+
 #include "esp_rom_gpio.h"
 #include "driver/gpio.h"
 #include "TFT_eSPI.h"
-#include <EEPROM.h>
-#include <StreamUtils.h>
-#include <string.h>
 
 WiFiMulti wifiMulti;
 
@@ -24,37 +25,23 @@ DynamicJsonDocument docSorted(2048);
 void setup() {
   String userId = "";
 
-  // Start Serial
   Serial.begin(115200);
-  Serial.printf("\n\n\n\n\n");
-  
-  // Start Screen
   tft.init();
   tft.setRotation(3);
-
-  // Start EEPROM
   EEPROM.begin(1000);
-
-  // Configure HTTP
   http.setCookieJar(&cookieJar);
   http.setReuse(true);
 
-  // Define Networks here
   wifiMulti.addAP("Felix", "idkidkidk");
-  // wifiMulti.addAP("DeathStar","coffeemarantz");
+  wifiMulti.addAP("DeathStar","coffeemarantz");
   WiFi.setHostname("classpill");
 
   tft.fillScreen(TFT_BLACK);
   tft.drawCentreString("Connecting Wifi...",64,64,1);
-  Serial.printf("Connecting Wifi...\n");
 
   if(wifiMulti.run() == WL_CONNECTED) {
     tft.fillScreen(TFT_BLACK);
     tft.drawCentreString("Wifi Connected!",64,64,1);
-    Serial.printf("WiFi connected!\n");
-    Serial.printf("IP address: ");
-    Serial.println(WiFi.localIP());
-    Serial.printf("\n");
 
     // Auth with Compass API
     tft.fillScreen(TFT_BLACK);
@@ -65,13 +52,11 @@ void setup() {
 
     if(httpCodeA > 0) {
       if(httpCodeA == HTTP_CODE_OK) {
-        // Convert response to JSON
         String payload = http.getString();
         DynamicJsonDocument doc(2048);
         deserializeJson(doc, payload);
-        // Get and print userId
         userId = doc["d"]["roles"][0]["userId"].as<String>();
-        Serial.printf("User ID: %s\n\n",userId);
+        Serial.printf("User ID: %s\n",userId);
       } else {
         Serial.printf("%i\n",httpCodeA);
       }
@@ -91,31 +76,29 @@ void setup() {
       if(httpCodeB > 0) {
         if(httpCodeB == HTTP_CODE_OK) {
           String payload = http.getString();
-          // Filter and deserialise JSON
           StaticJsonDocument<64> filter;
           filter["d"]["data"][0]["topAndBottomLine"] = true;
           DynamicJsonDocument doc(2048);
           deserializeJson(doc, payload, DeserializationOption::Filter(filter));
           JsonArray arr = doc["d"]["data"].as<JsonArray>();
+
           std::vector<String> values;
-          // Print to serial
           for (JsonVariant period : arr) {
             String s = (String)period["topAndBottomLine"].as<const char*>();
             values.push_back(s);
           }
-          // Sort periods
           std::sort(values.begin(), values.end(), [](const String& a, const String& b) {
-            // Split each string at the '-' character and convert the second part to an integer
             int aVal = a.substring(a.indexOf('-') + 1).toInt();
             int bVal = b.substring(b.indexOf('-') + 1).toInt();
-
-            // Compare the integer values
             return aVal < bVal;
           });
-          // Save to new arr
           periods = docSorted.to<JsonArray>();
+
           for(const String& value : values) {
             String periodString = value;
+            periodString.replace("<s>", "(");
+            periodString.replace("</s>", ")");
+            periodString.replace("&nbsp;", "");
             String delimiter = " - ";
             DynamicJsonDocument docPeriod(2048);
             JsonArray period = docPeriod.to<JsonArray>();
@@ -130,7 +113,7 @@ void setup() {
             period.add(periodString);
             periods.add(period);
           }
-          // Save to memory
+
           EepromStream eepromStream(0, 2048);
           serializeJson(periods, eepromStream);
           eepromStream.flush();
@@ -167,7 +150,7 @@ void loop(){
 
       spr.setTextColor(TFT_BLACK);
       spr.fillRect(0,0,128,18,TFT_WHITE);
-      spr.drawRightString(("Period "+(String)period[1].as<const char*>()),123,5,1);
+      spr.drawRightString((String)period[1].as<const char*>(),123,5,1);
       if(WiFi.isConnected()){
         spr.drawString(WiFi.SSID(),5,5,1);
       } else {
@@ -177,7 +160,6 @@ void loop(){
       spr.setTextColor(TFT_WHITE);
       spr.drawCentreString((String)period[0].as<const char*>(),64,34,2);
       spr.drawCentreString((String)period[2].as<const char*>(),64,54,2);
-      // /\<[^()]*\ /g
       spr.drawCentreString((String)period[4].as<const char*>(),64,74,2);
       spr.drawCentreString((String)period[3].as<const char*>(),64,94,2);
 
